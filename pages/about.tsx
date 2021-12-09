b3c758496b1ffc51a6909567462f72bd913132ed
Julian Kamphausen <juliankamphausen@me.com>
2021-12-09 21:55:54 +0100

    update

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import Footer from '../components/Footer'
+import Footer from '../components/footer'
 import Header from '../components/header'
 import styles from '../styles/Base.module.css'
 
